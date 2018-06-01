16830fe8a0a52ffbdf2bc71d69b4cf88274726aa
Marek Bel <marek.bel@posta.cz>
2018-06-01 15:45:33 +0200

    Try to inline to save memory. Flash is even 14 B worse. Ram no change.

--- Firmware/Timer.h
+++ Firmware/Timer.h
@@ -6,6 +6,8 @@
 #ifndef TIMER_H
 #define TIMER_H
 
+#include "Arduino.h"
+
 /**
  * @brief simple timer
  *
@@ -26,6 +28,64 @@ private:
     bool m_isRunning;
     T m_started;
 };
+
+/**
+ * @brief construct Timer
+ *
+ * It is guaranteed, that construction is equivalent with zeroing all members.
+ * This property can be exploited in MenuData union.
+ */
+template<typename T>
+Timer<T>::Timer() : m_isRunning(false), m_started()
+{
+}
+
+/**
+ * @brief Start timer
+ */
+template<typename T>
+void Timer<T>::start()
+{
+    m_started = millis();
+    m_isRunning = true;
+}
+
+/**
+ * @brief Timer has expired
+ *
+ * Timer is considered expired after msPeriod has passed from time the timer was started.
+ * Timer is stopped after expiration.
+ * This function must be called at least each (T maximum value - msPeriod) milliseconds to be sure to
+ * catch first expiration.
+ * This function is expected to handle wrap around of time register well.
+ *
+ * @param msPeriod Time interval in milliseconds. Do not omit "ul" when using constant literal with LongTimer.
+ * @retval true Timer has expired
+ * @retval false Timer not expired yet, or is not running, or time window in which is timer considered expired passed.
+ */
+template<typename T>
+bool Timer<T>::expired(T msPeriod)
+{
+    if (!m_isRunning) return false;
+    bool expired = false;
+    const T now = millis();
+    if (m_started <=  m_started + msPeriod)
+    {
+        if ((now >= m_started + msPeriod) || (now < m_started))
+        {
+            expired = true;
+        }
+    }
+    else
+    {
+        if ((now >= m_started + msPeriod) && (now < m_started))
+        {
+            expired = true;
+        }
+    }
+    if (expired) m_isRunning = false;
+    return expired;
+}
 
 /**
  * @brief Timer unsigned long specialization
